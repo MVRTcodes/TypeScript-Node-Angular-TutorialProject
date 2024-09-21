501cec8d5d8cee3880936db0b8b08c9fe4af3f2a
MVRT <alj.martinez.j@gmail.com>
2024-09-21 13:07:08 +0200

    lesson 2

--- app.ts
+++ app.ts
@@ -1,8 +1,13 @@
-"use strict";
 const character = 'luigi';
+
 console.log(character);
+
 const inputs = document.querySelectorAll('input');
+
 console.log(inputs);
+
 inputs.forEach(input => {
+
     console.log(input);
-});
+
+})
