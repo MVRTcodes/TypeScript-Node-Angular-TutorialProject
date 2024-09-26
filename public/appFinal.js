7cbd12cc1726f3aad3ecb7e1d1c2aaaca7173d5b
MVRT <alj.martinez.j@gmail.com>
2024-09-26 18:05:41 +0200

    lesson 18

--- public/appFinal.js
+++ public/appFinal.js
@@ -6,6 +6,9 @@ const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -15,12 +18,28 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    list.render(doc, type.value, 'start');
+    list.render(doc, type.value, 'end');
     console.log(doc);
 });
-// list template instance
-const ul = document.querySelector('ul');
-const list = new ListTemplate(ul);
+//Generics
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yoshi', age: 40 });
+console.log(docOne);
+const docTwo = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'shaun' }
+};
+const docThree = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk', 'toilet roll']
+};
+console.log(docTwo);
+console.log(docThree);
 //interfaces
 // interface IsPerson {
 //     name: string;
--- src/appFinal.ts
+++ src/appFinal.ts
@@ -11,6 +11,11 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+// list template instance
+
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
@@ -26,10 +31,37 @@ form.addEventListener('submit', (e: Event) => {
     console.log(doc);
 })
 
-// list template instance
+//Generics
 
-const ul = document.querySelector('ul')!;
-const list = new ListTemplate(ul);
+const addUID = <T>(obj: object) => {
+    let uid = Math.floor(Math.random() * 100);
+    return {...obj, uid};
+}
+
+let docOne = addUID({name: 'yoshi', age: 40});
+
+console.log(docOne);
+
+interface Resource<T> {
+    uid: number;
+    resourceName: string;
+    data: T;
+}
+
+const docTwo: Resource<object> = {
+    uid: 1,
+    resourceName: 'person',
+    data: {name: 'shaun'}
+}
+
+const docThree: Resource<string[]> = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: ['bread','milk','toilet roll']
+}
+
+console.log(docTwo);
+console.log(docThree);
 
 //interfaces
 
